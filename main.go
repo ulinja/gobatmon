cd8a14365c630e7f3e3acac89491e32495989d29
Julian Lobbes <julian@lobbes.dev>
2024-12-10 17:52:15 +0100

    add basic capacity check

--- main.go
+++ main.go
@@ -82,14 +82,105 @@ func showNotification(n notification) {
 	exitOnError(err, "Invocation of notify-send failed")
 }
 
+type runtimeConfig struct {
+	pollRate                           uint
+	normalWarningPercentageThreshold   uint
+	criticalWarningPercentageThreshold uint
+	normalWarningReminderTimeout       uint
+	criticalWarningReminderTimeout     uint
+}
+
+const (
+	defaultPollRate                           = 30
+	defaultNormalWarningPercentageThreshold   = 20
+	defaultCriticalWarningPercentageThreshold = 10
+	defaultNormalWarningReminderTimeout       = 15 * 60
+	defaultCriticalWarningReminderTimeout     = 5 * 60
+)
+
+func logValidationWarning(msg string) {
+	log.Printf("WARNING: %s. Using default value(s) instead.\n", msg)
+}
+
+// cleanRuntimeConfig validates, cleans and returns the provided runtime configuration.
+// If a validation error is encountered in the provided configuration, an error message is
+// logged and the affected configuration option is set to its default value as a fallback.
+func cleanRuntimeConfig(config runtimeConfig) (cleanedConfig runtimeConfig) {
+
+	cleanedConfig = config
+
+	if cleanedConfig.pollRate < 1 {
+		logValidationWarning("Poll Rate cannot be smaller than 1s")
+		cleanedConfig.pollRate = defaultPollRate
+	}
+
+	if t := cleanedConfig.normalWarningPercentageThreshold; t > 100 {
+		logValidationWarning("Normal Warning Percentage Threshold cannot exceed 100%")
+		cleanedConfig.normalWarningPercentageThreshold = defaultNormalWarningPercentageThreshold
+	} else if t < 0 {
+		logValidationWarning("Normal Warning Percentage Threshold cannot be less than 0%")
+		cleanedConfig.normalWarningPercentageThreshold = defaultNormalWarningPercentageThreshold
+	}
+
+	if t := cleanedConfig.criticalWarningPercentageThreshold; t > 100 {
+		logValidationWarning("Critical Warning Percentage Threshold cannot exceed 100%")
+		cleanedConfig.criticalWarningPercentageThreshold = defaultCriticalWarningPercentageThreshold
+	} else if t < 0 {
+		logValidationWarning("Critical Warning Percentage Threshold cannot be less than 0%")
+		cleanedConfig.criticalWarningPercentageThreshold = defaultCriticalWarningPercentageThreshold
+	}
+
+	if cleanedConfig.criticalWarningPercentageThreshold >= cleanedConfig.normalWarningPercentageThreshold {
+		logValidationWarning("Critical Warning Percentage Threshold must be smaller than Normal Warning Percentage Threshold")
+		cleanedConfig.normalWarningPercentageThreshold = defaultNormalWarningPercentageThreshold
+		cleanedConfig.criticalWarningPercentageThreshold = defaultCriticalWarningPercentageThreshold
+	}
+
+	if cleanedConfig.normalWarningReminderTimeout < 1 {
+		logValidationWarning("Normal Warning Reminder Timeout cannot be less than 1s")
+		cleanedConfig.normalWarningReminderTimeout = defaultNormalWarningReminderTimeout
+	}
+	if cleanedConfig.criticalWarningReminderTimeout < 1 {
+		logValidationWarning("Critical Warning Reminder Timeout cannot be less than 1s")
+		cleanedConfig.criticalWarningReminderTimeout = defaultCriticalWarningReminderTimeout
+	}
+
+	return
+}
+
+// runCapacityCheck executes a battery capacity check, reading the current capacity from the filesystem,
+// comparing it to the configured threshold values and triggering a notification if appropriate.
+func runCapacityCheck(config *runtimeConfig) {
+	// TODO: return the next scheduled run's timestamp as a value
+
+	capacity := uint(readCapacity())
+	if capacity <= config.criticalWarningPercentageThreshold {
+		showNotification(notification{
+			urgency: urgencyLevelCritical,
+			summary: "Very Low Battery",
+			body:    fmt.Sprintf("Charge is at %d%%.", capacity),
+		})
+	} else if capacity <= config.normalWarningPercentageThreshold {
+		showNotification(notification{
+			urgency: urgencyLevelNormal,
+			summary: "Low Battery",
+			body:    fmt.Sprintf("Charge is at %d%%.", capacity),
+		})
+	}
+}
+
 func main() {
 	assertDependenciesInstalled()
 
-	capacity := readCapacity()
-
-	n := notification{
-		summary: "Current Battery Capacity",
-		body:    fmt.Sprintf("%d%%", capacity),
+	// Just use defaults for now
+	config := runtimeConfig{
+		pollRate:                           defaultPollRate,
+		normalWarningPercentageThreshold:   defaultNormalWarningPercentageThreshold,
+		criticalWarningPercentageThreshold: defaultCriticalWarningPercentageThreshold,
+		normalWarningReminderTimeout:       defaultNormalWarningReminderTimeout,
+		criticalWarningReminderTimeout:     defaultCriticalWarningPercentageThreshold,
 	}
-	showNotification(n)
+	config = cleanRuntimeConfig(config)
+
+	runCapacityCheck(&config)
 }
