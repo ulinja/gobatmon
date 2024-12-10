ffa35593ce512ccf15d6d27980b33854f7aa5568
Julian Lobbes <julian@lobbes.dev>
2024-12-10 20:49:55 +0100

    implement continuous checking

--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"os/exec"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const pathToCapacityFile = "/sys/class/power_supply/BAT0/capacity"
+const pathToBatteryStateFile = "/sys/class/power_supply/BAT0/status"
 
 // exitOnError logs a fatal error message and exits the program with the provided prefix if error is not nil.
 func exitOnError(err error, prefix string) {
@@ -32,10 +34,46 @@ func readCapacity() (capacity uint64) {
 	exitOnError(err, "Failed to read battery capacity file")
 
 	text := strings.TrimSuffix(string(bytes), "\n")
-	currentCapacityPercent, err := strconv.ParseUint(text, 10, 64)
+	capacity, err = strconv.ParseUint(text, 10, 64)
 	exitOnError(err, "Failed to parse battery capacity file")
 
-	return currentCapacityPercent
+	if capacity > 100 {
+		log.Printf("WARNING: system reported battery capacity greater than 100 at %s: %d\n", pathToCapacityFile, capacity)
+		capacity = 100
+	} else if capacity < 0 {
+		exitOnError(fmt.Errorf("capacity: %d", capacity), "Battery capacity is negative")
+	}
+
+	return
+}
+
+type batteryState uint
+
+const (
+	batteryStateDischarging = iota
+	batteryStateCharging
+	batteryStateFull
+)
+
+// readBatteryState reads the battery state file and returns the current charge state.
+func readBatteryState() (batteryState batteryState) {
+	bytes, err := os.ReadFile(pathToBatteryStateFile)
+	exitOnError(err, "Failed to read battery state file")
+
+	text := strings.TrimSuffix(string(bytes), "\n")
+	switch text {
+	case "Discharging":
+		batteryState = batteryStateDischarging
+	case "Charging":
+		batteryState = batteryStateCharging
+	case "Full":
+		batteryState = batteryStateFull
+	default:
+		err = fmt.Errorf("unhandled status value: '%s'", text)
+		exitOnError(err, "Failed to parse battery state from file")
+	}
+
+	return
 }
 
 // assertDependenciesInstalled checks if all executable dependencies are present on $PATH, and terminates
@@ -91,10 +129,10 @@ type runtimeConfig struct {
 }
 
 const (
-	defaultPollRate                           = 30
+	defaultPollRate                           = 60
 	defaultNormalWarningPercentageThreshold   = 20
 	defaultCriticalWarningPercentageThreshold = 10
-	defaultNormalWarningReminderTimeout       = 15 * 60
+	defaultNormalWarningReminderTimeout       = 10 * 60
 	defaultCriticalWarningReminderTimeout     = 5 * 60
 )
 
@@ -148,25 +186,61 @@ func cleanRuntimeConfig(config runtimeConfig) (cleanedConfig runtimeConfig) {
 	return
 }
 
-// runCapacityCheck executes a battery capacity check, reading the current capacity from the filesystem,
+// runCheck executes a battery capacity check, reading the current capacity from the filesystem,
 // comparing it to the configured threshold values and triggering a notification if appropriate.
-func runCapacityCheck(config *runtimeConfig) {
-	// TODO: return the next scheduled run's timestamp as a value
+func runCheck(config *runtimeConfig) (capacity uint, batteryState batteryState) {
+	capacity = uint(readCapacity())
+	batteryState = readBatteryState()
 
-	capacity := uint(readCapacity())
-	if capacity <= config.criticalWarningPercentageThreshold {
-		showNotification(notification{
-			urgency: urgencyLevelCritical,
-			summary: "Very Low Battery",
-			body:    fmt.Sprintf("Charge is at %d%%.", capacity),
-		})
-	} else if capacity <= config.normalWarningPercentageThreshold {
-		showNotification(notification{
-			urgency: urgencyLevelNormal,
-			summary: "Low Battery",
-			body:    fmt.Sprintf("Charge is at %d%%.", capacity),
-		})
+	if batteryState == batteryStateDischarging {
+		if capacity <= config.criticalWarningPercentageThreshold {
+			showNotification(notification{
+				urgency: urgencyLevelCritical,
+				summary: "Very Low Battery",
+				body:    fmt.Sprintf("Charge is at %d%%.", capacity),
+			})
+		} else if capacity <= config.normalWarningPercentageThreshold {
+			showNotification(notification{
+				urgency: urgencyLevelNormal,
+				summary: "Low Battery",
+				body:    fmt.Sprintf("Charge is at %d%%.", capacity),
+			})
+		}
 	}
+
+	return
+}
+
+type programState struct {
+	batteryState batteryState
+	capacity     uint
+	lastCheck    time.Time
+}
+
+// durationUntilNextCheck calculates and returns the duration until the next battery state check, which depends
+// on the current charging status and the configured timeouts and polling rate.
+func durationUntilNextCheck(config *runtimeConfig, programState *programState) (duration time.Duration) {
+	switch programState.batteryState {
+	case batteryStateDischarging:
+		break
+	default:
+		return time.Duration(config.pollRate) * time.Second
+	}
+
+	durationSinceLastChecked := time.Now().Sub(programState.lastCheck)
+	if c := programState.capacity; c > config.normalWarningPercentageThreshold {
+		duration = time.Duration(config.pollRate) * time.Second
+	} else if c > config.criticalWarningPercentageThreshold {
+		duration = time.Duration(config.normalWarningReminderTimeout)*time.Second - durationSinceLastChecked
+	} else {
+		duration = time.Duration(config.criticalWarningReminderTimeout)*time.Second - durationSinceLastChecked
+	}
+
+	if duration < 0 {
+		duration = 0
+	}
+
+	return
 }
 
 func main() {
@@ -178,9 +252,19 @@ func main() {
 		normalWarningPercentageThreshold:   defaultNormalWarningPercentageThreshold,
 		criticalWarningPercentageThreshold: defaultCriticalWarningPercentageThreshold,
 		normalWarningReminderTimeout:       defaultNormalWarningReminderTimeout,
-		criticalWarningReminderTimeout:     defaultCriticalWarningPercentageThreshold,
+		criticalWarningReminderTimeout:     defaultCriticalWarningReminderTimeout,
 	}
 	config = cleanRuntimeConfig(config)
 
-	runCapacityCheck(&config)
+	var programState programState
+
+	for {
+		capacity, batteryState := runCheck(&config)
+		programState.capacity = capacity
+		programState.batteryState = batteryState
+		programState.lastCheck = time.Now()
+
+		durationUntilNextCheck := durationUntilNextCheck(&config, &programState)
+		time.Sleep(durationUntilNextCheck)
+	}
 }
